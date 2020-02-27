6075b6fbcf9e6520452f73c9bd519f16fe7e6c82
Olimpio Kaio <olimpiocode@gmail.com>
2020-02-27 09:15:36 -0300

    atualização de funcionalidades e estilos do modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -14,6 +14,11 @@ const GoalInput = props => {
         setEnteredGoal(enteredText);
     }
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    }
+
     return (
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.iputContener}>
@@ -23,7 +28,14 @@ const GoalInput = props => {
                     onChangeText={goalInputHandler}
                     value={enteredGoal}
                 />
-                <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)}/>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="CANCEL" color="red" onPress={props.onCancel}/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="ADD" onPress={addGoalHandler}/>
+                    </View>
+                </View>
             </View>
         </Modal>
     );
@@ -31,16 +43,25 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
     input: {
+        width: '80%',
         borderColor: 'black', 
         borderWidth: 1, 
-        padding: 10, 
-        width: '80%'
+        padding: 10,
+        marginBottom: 10, 
     },
-    iputContener: { 
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
+    iputContener: {
+        flex: 1,
+        justifyContent: 'center', 
         alignItems: 'center' 
-      }
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '60%'
+    },
+    button: {
+        width: '40%'
+    }
 })
 
 export default GoalInput;
